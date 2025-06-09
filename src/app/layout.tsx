2357cec40e7b6f03dfa1e08aa30fcc6371fdc66f
BlessedNur <blessednur67@gmail.com>
2025-06-09 21:12:53 +0100

    Add Tawk.to live chat integration to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,19 @@ export default function RootLayout({
         {children}
 
         {/* Tawk.to Live Chat Script */}
+        <Script id="tawkto-script" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68473ef8d96b7b190b77b63c/1itb4rutt';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
